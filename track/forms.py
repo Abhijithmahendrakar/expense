a817a05ccc9d5d87b4b5486eee152220a15c4d97
abhijithms <abhijithmahendrakar562@gmail.com>
2019-05-22 00:58:15 +0530

    feat : form changed changes are done

--- track/forms.py
+++ track/forms.py
@@ -9,5 +9,11 @@ class NewItemModelForm(forms.ModelForm):
 		model = NewItem
 		fields = ('name', 'price', 'image')
 
+#ThisForm is for the User Registration
+class SignUpForm(UserCreationForm):
+		email = forms.EmailField(max_length = 254, help_text = 'Enter a valid Email address.')
+		password = forms.CharField(widget = forms.PasswordInput, help_text = 'MIN 8 chars,should include special char,alpha-numeric char')
 
-
+		class Meta:
+			model = User
+			fields = ('Username', 'Email', 'Password', 'Password1')	

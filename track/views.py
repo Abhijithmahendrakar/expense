17deca8268c12cdf63c9da31aba532903a7a8530
abhijithms <abhijithmahendrakar562@gmail.com>
2019-05-22 17:54:26 +0530

    feat : sort by price display items by price sort

--- track/views.py
+++ track/views.py
@@ -70,10 +70,15 @@ def update(request,id):
 
 #deleting items by primary key on click    
 def delete(request,id):                                    
-    deleteitem=get_object_or_404(Item,pk=id).delete()
+    deleteitem=get_object_or_404(NewItem,pk=id).delete()
     return HttpResponseRedirect('/display')
 
 #sorting by name
 def sortbyname(request):                                  
     sname=NewItem.objects.order_by('name').all()
     return render(request,"track/sname.html",{'name':sname})
+
+#sorting by price    
+def sortbyprice(request):                                 
+    sprice=NewItem.objects.order_by('price').all()
+    return render(request,"track/sprice.html",{'price':sprice})

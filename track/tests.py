f0f31799acc0c2c633642e5b514a6990db16e2cb
abhijithms <abhijithmahendrakar562@gmail.com>
2019-05-23 22:08:54 +0530

    added end to end testing using selenium

--- track/tests.py
+++ track/tests.py
@@ -59,14 +59,14 @@ class UserFormTest(TestCase):
 	#testing validation
     def test_valid_form(self):     
         w = NewItem.objects.create(name='bike', price=10000)
-        data = {'username':" ", 'email':" ",'password1':" ",'password2':" "}
+        data = {'username':"abhijithms ", 'email':"abhijithms@gmail.com ",'password1':"qazxwscv ",'password2':"qazxwscv "}
         form = SignUpForm(data=data)
         self.assertTrue(form.is_valid())
 
     #invalid form testing using false details    
     def test_invalid_form(self):   
         w = NewItem.objects.create(name='bike', price=10000)
-        data = {'username': " ", 'email': " ", 'password1': " ", 'password2': " "}
+        data = {'username': "abhijithms ", 'email': "abhijithms@gmail.com ", 'password1': "qazxwsc ", 'password2': "qazxwscv "}
         form = SignUpForm(data=data)
         self.assertFalse(form.is_valid())
 
@@ -84,3 +84,71 @@ class NewItemModelFormTest(TestCase):
         data = {'name': "Bike",}
         form = NewItemModelForm(data=data)
         self.assertFalse(form.is_valid())
+
+#change the values of usename , password, email everytime you test here and in line no: 140 
+class EndtoEndTest(LiveServerTestCase): #End to End testing using Selenium
+    username = "harsha"                   #sample name
+    email = "harsha@gmail.com"            #email
+    password = "simple1234"             #and password
+
+    driver = webdriver.Chrome('C:\\Users\\abhi\\Desktop\\proj\\chromedriver') #load chrome driver from local system
+
+    driver.get('http://127.0.0.1:8000/signup/') #Local host
+
+    assert "Signup" in driver.title
+
+    elem = driver.find_element_by_id("id_username") #finds elements by id and enter the data
+    elem.send_keys(username)
+
+    elem = driver.find_element_by_id("id_email")
+    elem.send_keys(email)
+
+    elem = driver.find_element_by_id("id_password1")
+    elem.send_keys(password)
+
+    elem = driver.find_element_by_id("id_password2")
+    elem.send_keys(password)
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)                     #Press enter key
+
+    elem = driver.find_element_by_id("id_name")     #add items page , enter the credentials
+    elem.send_keys("television")
+    elem = driver.find_element_by_id("id_price")
+    elem.send_keys(1000)
+    driver.find_element_by_id("id_image").send_keys(os.getcwd() + "/download.jpg") #give path to image from root during testing
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)
+    time.sleep(2)
+    elem = driver.find_element_by_id("id_name")
+    elem.send_keys("television")
+    elem = driver.find_element_by_id("id_price")
+    elem.send_keys(1000)
+    driver.find_element_by_id("id_image").send_keys(os.getcwd() + "/download.jpg") #give path to image from root during testing
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)
+    time.sleep(2)
+    elem = driver.find_element_by_id("id_name")
+    elem.send_keys("television")
+    elem = driver.find_element_by_id("id_price")
+    elem.send_keys(1000)
+    driver.find_element_by_id("id_image").send_keys(os.getcwd() + "/download.jpg") #give path to image from root during testing
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)
+    time.sleep(2)
+    driver.find_element_by_css_selector('.button.button2').click() #verify all filters
+    time.sleep(2)
+    driver.find_element_by_css_selector('.button.button5').click()
+    time.sleep(2)
+    driver.find_element_by_css_selector('.button.button5').click()
+    link = driver.find_element_by_link_text('Logout')
+    time.sleep(2)
+    link.click()
+    link = driver.find_element_by_link_text('Login') #testing login page using creditials from Signup
+    link.click()
+    elem = driver.find_element_by_id("id_username") 
+    elem.send_keys(username) 
+    elem = driver.find_element_by_id("id_password")
+    elem.send_keys(password)
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)
+    driver.close() #Closes the Chrome browser

154f7b529e1c22119a12bbba60154d54eefcc805
abhijithms <abhijithmahendrakar562@gmail.com>
2019-05-24 02:17:28 +0530

    modified

--- expense/settings.py
+++ expense/settings.py
@@ -126,15 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
-)
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 LOGIN_REDIRECT_URL  =  '/item/'
